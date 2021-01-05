52f6f48c605fc84b518f267da8ad50b5c32d6b9f
divergent-rose <76853316+divergent-rose@users.noreply.github.com>
2021-01-04 20:38:42 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "my-project-89796-terraform"
+  region      = "us-central1-a"
+}
+resource "google_compute_firewall" "default" {
+  name    = "demo-firewall"
+  network = google_compute_network.default.name
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+  source_tags = ["web"]
+}
+resource "google_compute_network" "default" {
+  name = "demo-network"
+}
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "ubuntuk-vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+  // Make sure flask is installed on all new instances for later steps
+  metadata_startup_script = "sudo apt-get -y update; sudo apt-get install -y dist-upgrade; sudo apt-get -y install nginx"
+  network_interface {
+    network = "default"
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+}
